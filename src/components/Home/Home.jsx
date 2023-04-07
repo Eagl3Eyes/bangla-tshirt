259f4636caf19fe351928eb5bf4a9231c18d27d2
Eagl3Eyes <58110053+Eagl3Eyes@users.noreply.github.com>
2023-04-07 16:11:09 +0600

    data loaded

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
 
 const Home = () => {
     const tshirt = useLoaderData();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt => {
+
+    }
+
     return (
-        <div>
-            <h2>This is Home: {tshirt.length}</h2>
+        <div className='home-container'>
+            <div className='t-shirts-container'>
+                {
+                    tshirt.map(tshirt => <Tshirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
+                    ></Tshirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
---
+++ src/components/Tshirt/Tshirt.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Tshirt.css';
+
+const Tshirt = ({tshirt, handleAddToCart}) => {
+    const {picture, name, price} = tshirt;
+
+    return (
+        <div className='t-shirt'>
+            <img src={picture} alt="" />
+            <h4>{name}</h4>
+            <p>${price}</p>
+            <button onClick={handleAddToCart}>Buy Now</button>
+        </div>
+    );
+};
+
+export default Tshirt;
