52e9144a691fd9794165a5907eff0b237f77de3b
Eagl3Eyes <58110053+Eagl3Eyes@users.noreply.github.com>
2023-04-07 16:42:07 +0600

    cart added and toaster added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,14 +2,29 @@ import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Tshirt from '../Tshirt/Tshirt';
 import Cart from '../Cart/Cart';
-import './Home.css'
+import './Home.css';
+import toast from 'react-hot-toast';
 
 const Home = () => {
     const tshirt = useLoaderData();
     const [cart, setCart] = useState([]);
 
     const handleAddToCart = tshirt => {
+        const exists = cart.find(ts => ts._id === tshirt._id);
+        if (exists) {
+            toast.error('You have already added this t-shirt')
+        }
+        else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+            toast.success('Successfully Added!')
+        }
+    }
 
+
+    const handleRemoveFromCart = id => {
+        const remaining = cart.filter(ts => ts._id !== id);
+        setCart(remaining);
     }
 
     return (
@@ -24,7 +39,10 @@ const Home = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart
+                    cart={cart}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
             </div>
         </div>
     );
