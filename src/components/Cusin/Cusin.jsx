6fb00b4df5342d0d68c11f5201f89cb04ffd6492
Eagl3Eyes <58110053+Eagl3Eyes@users.noreply.github.com>
2023-04-07 17:49:32 +0600

    pass data

---
+++ src/components/Cusin/Cusin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Friend from '../Friend/Friend';
+
+const Cusin = ({children, hasFriend, ring}) => (
+    <div>
+        <h2>Cousin</h2>
+        <p><small>{children}</small></p>
+        { hasFriend && <Friend ring={ring}></Friend>}
+    </div>
+);
+
+export default Cusin;
